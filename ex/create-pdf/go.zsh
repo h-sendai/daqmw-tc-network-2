95887976f1c7dea6fad23daed4a176be47eab6d1
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2014-08-19 11:01:59 +0900

    update for ex14

--- ex/create-pdf/go.zsh
+++ ex/create-pdf/go.zsh
@@ -3,6 +3,8 @@
 setopt extendedglob
 setopt nonomatch
 
+last_ex=14
+
 export PATH=/usr/local/texlive/2013/bin/x86_64-linux:$PATH
 
 rm -f ex??.tex
@@ -13,11 +15,7 @@ rm -fr *.log
 
 pandoc -t latex ../README.md -o ex.tex
 
-for i in {01..13}; do
-    pandoc -t latex ../ex${i}/README.md -o ex${i}.tex
-done
-
-for i in {01..13}; do
+for i in {01..$last_ex}; do
     pandoc -t latex ../ex${i}/README.md -o ex${i}.tex
 done
 

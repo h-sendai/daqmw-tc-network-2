52a8f9960720586a96c3c5d4502ae72b6436850d
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2018-09-26 09:55:50 +0900

    Makefile

---
+++ ex/ex01/basic/sample.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "world.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "hello" << endl;
+
+    world(3);
+
+    return 0;
+}

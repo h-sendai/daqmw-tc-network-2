bcfd8cdd9a22677d0e83ecf77b8a8a39d6999f22
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2020-09-15 15:55:36 +0900

    indent

--- ex/ex02/MyClass.cpp
+++ ex/ex02/MyClass.cpp
@@ -3,13 +3,13 @@
 // Default Constructor
 MyClass::MyClass(): m_x(0), m_y(0)
 {
-	std::cerr << "MyClass default ctor()" << std::endl;
+    std::cerr << "MyClass default ctor()" << std::endl;
 }
 
 // Constructor with two arguments
 MyClass::MyClass(int x, int y): m_x(x), m_y(y)
 {
-	std::cerr << "MyClass ctor(int, int)" << std::endl;
+    std::cerr << "MyClass ctor(int, int)" << std::endl;
 }
 
 MyClass::MyClass(const MyClass &rhs): m_x(rhs.m_x), m_y(rhs.m_y)
@@ -30,27 +30,27 @@ MyClass& MyClass::operator =(const MyClass& rhs)
 // Destructor
 MyClass::~MyClass()
 {
-	std::cerr << "MyClass dtor()" << std::endl;
+    std::cerr << "MyClass dtor()" << std::endl;
 }
 
 int MyClass::set_x(int x)
 {
-	m_x = x;
-	return 0;
+    m_x = x;
+    return 0;
 }
 
 int MyClass::set_y(int y)
 {
-	m_y = y;
-	return 0;
+    m_y = y;
+    return 0;
 }
 
 int MyClass::get_x()
 {
-	return m_x;
+    return m_x;
 }
 
 int MyClass::get_y()
 {
-	return m_y;
+    return m_y;
 }
